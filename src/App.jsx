bfa5f0a1ca175a7ddb46d77126f1829ff0818ff1
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-22 16:11:06 +0100

    new tag commit message

--- src/App.jsx
+++ src/App.jsx
@@ -38,23 +38,31 @@ const App = () => {
           <a href="#">Log in</a>
           <a href="#">Sign up</a>
         </div>
+
         <header>
           <h2>Welcome to Tailwind</h2>
           <h3>Your one-stop solution for modern web design</h3>
         </header>
+
         <div>
           <h4>Your go-to resource for Tailwind CSS</h4>
-          <div>
+         
             {/* Cards go here */}
+            <div>
             <img src="" alt="" srcset="" />
+            
                <div>
                 <span>Card Title</span>
                 <span>Card Description</span>
                </div>
           </div>
         </div>
-        <h4>Your go-to resource for Tailwind CSS</h4>
+        <div>
+          <h4>Your go-to resource for Tailwind CSS</h4>
+        </div>
+
        </main>
+       
     </div>
   )
 }

f08e430ff125bff4dd13999934ccb8056d53001f
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-23 14:49:54 +0100

    Changed the py-16 to py-6

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
          </nav>
       </div>  {/*---End of the navigation bar---*/}
 
-       <main className='px-16 py-16' >
+       <main className='px-16 py-6' >
         <div>
           <a href="#">Log in</a>
           <a href="#">Sign up</a>

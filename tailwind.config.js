f41b225f676453940abc8a98b56131645a3238f7
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-23 15:55:34 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8758ff", // Your favorite purple shade
+        secondary: "#ff58a6", // A complementary pink
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Add Poppins font
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      screens: {
+        xs: "480px", // Extra small screen
+      },
+    },
   },
   plugins: [],
 }
